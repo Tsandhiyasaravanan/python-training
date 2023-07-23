20069b7b43aa06c453c9ec463b36614be73f1848
Sandhiya Saravanan <sandhiyasaravanan126@gmail.com>
2023-07-23 17:07:42 +0530

    python class

---
+++ main.py
@@ -0,0 +1,18 @@
+print(type("sttt"))
+print(type(20.0))
+x=10
+print(x)
+x-=10
+print(x)
+x%=10
+print(x)
+x*=10
+print(x)
+x//=10
+print(x)
+x/=10
+print(x)
+x=10
+print(x<10 or x>=10)
+print(x>10 and x!=100)
+print(not(x==10 and x>=100))
